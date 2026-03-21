da3f39b06606f11ebee6e762bad5b3ae87ba4f96
bhumo <bhumi.gupta89@gmail.com>
2026-03-20 23:40:55 -0400

    v.1

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='relative bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <StarsCanvas className='absolute inset-0 z-0' />
+          <div className='relative z-10'>
+            <Navbar />
+            <Hero />
+          </div>
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
